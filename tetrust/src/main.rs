7c1b10ee7af580ef7995c308cfb2b5dbef1ae694
Kaito Suzuki <snails8d@gmail.com>
2023-02-10 16:31:00 +0900

    add: block の落下判定追加

--- tetrust/src/main.rs
+++ tetrust/src/main.rs
@@ -1,3 +1,10 @@
+use std::{thread, time};
+
+struct Position {
+  x: usize,
+  y: usize,
+}
+
 fn main() {
 
   let field = [
@@ -24,30 +31,51 @@ fn main() {
     [1,1,1,1,1,1,1,1,1,1,1,1,1],
   ];
 
+  let mut pos = Position { x: 4, y: 0 };
 
-  let mut field_buf = field; // mutable
-  for y in 0..4 {
-    for x in 0..4 {
-      field_buf[y+ 2][x+2] |= BLOCKS[BlockKind::I as usize][y][x];
-      field_buf[y+ 2][x+7] |= BLOCKS[BlockKind::O as usize][y][x];
-      field_buf[y+ 6][x+2] |= BLOCKS[BlockKind::S as usize][y][x];
-      field_buf[y+ 6][x+7] |= BLOCKS[BlockKind::Z as usize][y][x];
-      field_buf[y+10][x+2] |= BLOCKS[BlockKind::J as usize][y][x];
-      field_buf[y+10][x+7] |= BLOCKS[BlockKind::L as usize][y][x];
-      field_buf[y+14][x+2] |= BLOCKS[BlockKind::T as usize][y][x];
-    }
-  }
+  // 画面クリア
+  println!("\x1b[2J\x1b[H\x1b[?25l");
 
-  for y in 0..21 {
-    for x in 0..13 {
-      if field_buf[y][x] == 1 {
-        print!("[]");  // 
-      } else {
-        print!(" .")
+  // block info
+  for _ in 0..5 {
+    let mut field_buf = field; // mutable
+    for y in 0..4 {
+      for x in 0..4 {
+        // field_buf[y+ 2][x+2] |= BLOCKS[BlockKind::I as usize][y][x];
+        // field_buf[y+ 2][x+7] |= BLOCKS[BlockKind::O as usize][y][x];
+        // field_buf[y+ 6][x+2] |= BLOCKS[BlockKind::S as usize][y][x];
+        // field_buf[y+ 6][x+7] |= BLOCKS[BlockKind::Z as usize][y][x];
+        // field_buf[y+10][x+2] |= BLOCKS[BlockKind::J as usize][y][x];
+        // field_buf[y+10][x+7] |= BLOCKS[BlockKind::L as usize][y][x];
+        // field_buf[y+14][x+2] |= BLOCKS[BlockKind::T as usize][y][x];
+
+        if BLOCKS[BlockKind::I as usize][y][x] == 1 {
+          field_buf[y+pos.y][x+pos.x] = 1;
+        }
       }
     }
-    println!(); // 改行の役目
+
+    // update pos
+    pos.y += 1;
+
+    // show field
+    println!("\x1b[H");  // カーソルを先頭に移動
+    for y in 0..21 {
+      for x in 0..13 {
+        if field_buf[y][x] == 1 {
+          print!("[]");  // 
+        } else {
+          print!(" .")
+        }
+      }
+      println!(); // 改行の役目
+    }
+    
+    thread::sleep(time::Duration::from_millis(1000));
   }
+
+  // カーソルを再表示
+  println!("\x1b[?25h");
 }
 
 enum BlockKind {
